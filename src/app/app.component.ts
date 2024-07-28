242da679258690698b841a8b7487358c11943c11
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-28 21:35:25 +0800

    remove themeService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
-import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: "app-root",
@@ -10,8 +9,4 @@ import { ThemeService } from './services/theme.service';
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  themeService = inject(ThemeService);
-
-  theme = this.themeService.get();
-}
+export class AppComponent {}
