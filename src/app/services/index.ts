5a8d8173cabaaac32c686a5a7c719e8e71f8c2ce
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-28 21:36:30 +0800

    import and export services

---
+++ src/app/services/index.ts
@@ -0,0 +1,7 @@
+import { LocalStorageService } from "./localStorage/local-storage.service";
+import { ThemeService } from "./theme/theme.service";
+
+export {
+  LocalStorageService,
+  ThemeService
+}
