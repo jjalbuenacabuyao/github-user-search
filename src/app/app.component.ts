af0a82cacec633d9d888755b10632cedc1791d00
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-29 21:34:47 +0800

    add SearchUserComponent in imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
+import { SearchUserComponent } from "./search-user/search-user.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchUserComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
