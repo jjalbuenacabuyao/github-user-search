80f2d2ad68f0c937926fdd66cd7e832ef3e002b1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-30 22:47:35 +0800

    export GithubUserDataService

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,7 +1,9 @@
 import { LocalStorageService } from "./localStorage/local-storage.service";
 import { ThemeService } from "./theme/theme.service";
+import { GithubUserDataService } from "./githubUserData/github-user-data.service";
 
 export {
   LocalStorageService,
-  ThemeService
+  ThemeService,
+  GithubUserDataService
 }
