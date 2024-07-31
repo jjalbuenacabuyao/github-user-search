76d423d673e0ff43cf517ca6fb4b23f729fa13fc
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-31 22:58:01 +0800

    remove the logic of fetching component

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,20 +1,6 @@
 import { Component } from "@angular/core";
 import { GithubUserDataService } from "../services";
-
-type User = {
-  name: string;
-  username: string;
-  avatarUrl: string;
-  company: string;
-  blog: string;
-  bio: string;
-  location: string;
-  twitterUsername: string;
-  publicRepositories: number;
-  followers: number;
-  following: number;
-  joinedDate: string;
-};
+import { UserData } from "../services/githubUserData/github-user-data.service";
 
 @Component({
   selector: "app-user-card",
@@ -24,7 +10,7 @@ type User = {
   styleUrl: "./user-card.component.css",
 })
 export class UserCardComponent {
-  userData: User = {
+  userData: UserData = {
     name: "",
     username: "",
     avatarUrl: "",
@@ -38,66 +24,12 @@ export class UserCardComponent {
     following: 0,
     joinedDate: "",
   };
-  username = "jjalbuenacabuyao";
 
   constructor(private githubUserDataService: GithubUserDataService) {
     this.fetchUserData();
   }
 
   fetchUserData() {
-    this.githubUserDataService.getUserData(this.username).subscribe({
-      next: (data) => {
-        const {
-          login,
-          name,
-          avatar_url,
-          company,
-          blog,
-          location,
-          bio,
-          twitter_username,
-          public_repos,
-          followers,
-          following,
-          created_at,
-        } = data;
-
-        const joinedAt = created_at.split("T")[0];
-        const parsedJoinedAt = joinedAt.split("-");
-
-        const year = parsedJoinedAt[0];
-        const month = parsedJoinedAt[1];
-        const day = parsedJoinedAt[2];
-
-        // Converts month to shortened text version
-        const date = new Date(year, month, day);
-        date.setMonth(month - 1);
-
-        const monthTxt = date.toLocaleString("en", { month: "short" });
-
-        let extractedUserData: User = {
-          name: name,
-          username: `@${login}`,
-          avatarUrl: avatar_url,
-          company: company ? company : "Not available",
-          blog: blog ? blog : "Not available",
-          bio: bio ? bio : "Not available",
-          location: location ? location : "Not available",
-          twitterUsername: twitter_username
-            ? twitter_username
-            : "Not available",
-          publicRepositories: public_repos,
-          followers: followers,
-          following: following,
-          joinedDate: `${day} ${monthTxt} ${year}`,
-        };
-
-        this.userData = extractedUserData;
-        console.log(this.userData);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
+    this.userData = this.githubUserDataService.getUserData();
   }
 }
