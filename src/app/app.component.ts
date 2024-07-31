882e41e7490045b2e59d28fc371663584499e4e0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-31 22:56:03 +0800

    import ReactiveFormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { SearchUserComponent } from "./search-user/search-user.component";
 import { UserCardComponent } from "./user-card/user-card.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchUserComponent, UserCardComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchUserComponent, UserCardComponent, ReactiveFormsModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
