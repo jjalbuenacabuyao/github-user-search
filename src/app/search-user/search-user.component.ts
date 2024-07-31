0855ffcdf932f93933f105dd6ac964fd338530b0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-31 22:56:50 +0800

    add onSubmit function and validation

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { GithubUserDataService } from '../services';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-user.component.html',
   styleUrl: './search-user.component.css'
 })
 export class SearchUserComponent {
+  usernameForm: FormGroup;
 
+  constructor(private githubUserDataService: GithubUserDataService, private formBuilder: FormBuilder) {
+    this.usernameForm = this.formBuilder.group({
+      username: ["", [Validators.required]]
+    })
+  }
+
+  onSubmit() {
+    if(this.usernameForm.valid) {
+      this.githubUserDataService.setUsername(this.usernameForm.value.username);
+      console.log(this.usernameForm.value.username);
+    }
+  }
 }
